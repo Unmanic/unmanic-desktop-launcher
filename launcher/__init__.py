59b4ec5bf6ff3871531c446dade1897efa2a076b
Josh.5 <jsunnex@gmail.com>
2022-05-05 22:28:22 +1200

    Add menu items from AutoIT build

--- launcher/__init__.py
+++ launcher/__init__.py
@@ -6,12 +6,14 @@
 # File Created: Sunday, 13th March 2022 8:25:09 pm
 # Author: Josh Sunnex (jsunnex@gmail.com)
 # -----
-# Last Modified: Thursday, 28th April 2022 3:22:58 pm
+# Last Modified: Thursday, 5th May 2022 10:27:52 pm
 # Modified By: Josh.5 (jsunnex@gmail.com)
 ###
 import os
+import webbrowser
+
 import psutil
-import subprocess
+import requests
 
 from pystray import MenuItem as item
 import pystray
@@ -28,6 +30,7 @@ class UnmanicLauncher:
         self.unmanic_service = None
         self.icon = None
         self.unmanic_proc = None
+        self.other_procs = []
 
     @staticmethod
     def __terminate_proc_tree(proc: psutil.Process):
@@ -38,8 +41,11 @@ class UnmanicLauncher:
         :param proc:
         :return:
         """
-        children = proc.children(recursive=True)
-        children.append(proc)
+        try:
+            children = proc.children(recursive=True)
+            children.append(proc)
+        except psutil.NoSuchProcess:
+            return
         for p in children:
             try:
                 p.terminate()
@@ -53,12 +59,47 @@ class UnmanicLauncher:
                 pass
         psutil.wait_procs(alive, timeout=3)
 
-    def update_unmanic(self):
-        # TODO: Make this also cleaned up on shutdown
+    @staticmethod
+    def pause_all_workers():
+        """Pauses all workers"""
+        requests.post('http://localhost:8888/unmanic/api/v2/workers/worker/pause/all', json={}, timeout=1)
+
+    @staticmethod
+    def resume_all_workers():
+        """Resumes all workers"""
+        requests.post('http://localhost:8888/unmanic/api/v2/workers/worker/resume/all', json={}, timeout=1)
+
+    @staticmethod
+    def trigger_library_scanner():
+        """Triggers the library scanner"""
+        requests.post('http://localhost:8888/unmanic/api/v2/pending/rescan', json={}, timeout=1)
+
+    @staticmethod
+    def launch_browser():
+        """Opens the Unmanic frontend in the browser"""
+        webbrowser.open_new("http://localhost:8888")
+
+    def display_updater(self):
+        """Display the updater window"""
+        # TODO: Run without the subprocess and fetch the result
         update_command = [common.python_exe, '-m', 'launcher', '--updater']
         proc, sp = common.exec_process(update_command)
+        self.other_procs.append(proc)
+        # TODO: If an update was run, restart unmanic
+        # while not sp.poll() is not None:
+        #     time.sleep(.1)
+        # self.stop_unmanic()
+        # self.start_unmanic()
+
+    def display_about(self):
+        """Display the updater window"""
+        pass
 
     def start_unmanic(self):
+        """Start the main Unmanic service"""
+        if self.unmanic_proc is not None:
+            # Unmanic is already running
+            return
         try:
             # Ensure the module is installed
             import unmanic
@@ -68,16 +109,48 @@ class UnmanicLauncher:
         except ModuleNotFoundError:
             # Unmanic is not yet installed
             # Install it...
-            self.update_unmanic()
+            self.display_updater()
 
     def stop_unmanic(self):
+        """Terminate the main Unmanic process"""
         # Terminate Unmanic
         if self.unmanic_proc is not None and self.unmanic_proc.is_running():
             self.__terminate_proc_tree(self.unmanic_proc)
+        self.unmanic_proc = None
+
+    def restart_unmanic(self):
+        """Stop and restart the Unmanic process"""
+        self.stop_unmanic()
+        self.start_unmanic()
+
+    def stop_other_processes(self):
+        """Terminate all other processes that may still be running"""
+        for proc in self.other_procs:
+            self.__terminate_proc_tree(proc)
 
     def action_exit(self):
         self.icon.visible = False
         self.icon.stop()
+
+    def create_icon_menu(self):
+        return pystray.Menu(
+            item('Start/Stop Unmanic', pystray.Menu(
+                item('Start Unmanic', self.start_unmanic),
+                item('Stop Unmanic', self.stop_unmanic),
+                item('Restart Unmanic', self.restart_unmanic),
+            )),
+            item('Workers', pystray.Menu(
+                item('Pause All Workers', self.pause_all_workers),
+                item('Resume All Workers', self.resume_all_workers),
+            )),
+            item('Scan Libraries', self.trigger_library_scanner),
+            pystray.Menu.SEPARATOR,
+            item('Launch Browser', self.launch_browser),
+            item('Update', self.display_updater),
+            # item('About', self.display_about),
+            pystray.Menu.SEPARATOR,
+            item('Exit', self.action_exit)
+        )
 
     def create_icon(self):
         # When installed, the icon will be up a level
@@ -85,7 +158,8 @@ class UnmanicLauncher:
         if os.path.exists(os.path.join(common.module_root, 'assets', 'icon.ico')):
             # When running from this script
             image = Image.open(os.path.join(common.module_root, 'assets', 'icon.ico'))
-        self.icon = pystray.Icon("Unmanic", image, "Unmanic", (item('Exit', self.action_exit),))
+        menu = self.create_icon_menu()
+        self.icon = pystray.Icon("Unmanic", image, "Unmanic", menu)
 
     def run(self):
         # Create icon
@@ -96,6 +170,8 @@ class UnmanicLauncher:
         self.icon.run()
         # Stop Unmanic
         self.stop_unmanic()
+        # Stop other subprocesses
+        self.stop_other_processes()
 
 
 def main():
